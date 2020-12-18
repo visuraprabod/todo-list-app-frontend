1c9b1a47d2c1e9393aa4c15e834c21fc1bf32b50
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2020-12-18 11:55:13 +0530

    Revamp with a spearate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {TaskService} from './service/task.service';
+import { TaskItemComponent } from './task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule
